8466b870ce2c9bf0e5048dacef13d5701b1ee65f
janyocarvalho <janyo.medeiros@gmail.com>
2020-05-06 21:13:52 -0300

    iniciando nosso projeto

---
+++ index.js
@@ -0,0 +1,14 @@
+ O volume na unidade C n∆o tem nome.
+ O N£mero de SÇrie do Volume Ç 5459-945B
+
+ Pasta de C:\gitTesteClone
+
+06/05/2020  21:08    <DIR>          .
+06/05/2020  21:08    <DIR>          ..
+06/05/2020  20:50             1.714 .gitignore
+06/05/2020  21:08                 0 index.js
+06/05/2020  20:54    <DIR>          node_modules
+06/05/2020  20:54            14.289 package-lock.json
+06/05/2020  20:54               542 package.json
+               4 arquivo(s)         16.545 bytes
+               3 pasta(s)   94.279.000.064 bytes dispon°veis
